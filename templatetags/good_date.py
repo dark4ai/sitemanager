f3e07f2b5d30618e83748df4411c079a57c06f1f
Maxim Korystov <dark4ai@gmail.com>
2011-07-18 09:17:49 +0600

    update tagfilters and bug fixes

--- templatetags/good_date.py
+++ templatetags/good_date.py
@@ -4,14 +4,26 @@ from datetime import date
 
 register = Library()
 
+months = {1:u'января', 2:u'февраля', 3:u'марта', 4:u'апреля', 5:u'мая', 6:u'июня', 7:u'июля', 8:u'августа', 9:u'сентября', 10:u'октября', 11:u'ноября', 12:u'декабря'}
+
 def good_date(d):
-    months = {1:u'января', 2:u'февраля', 3:u'марта', 4:u'апреля', 5:u'мая', 6:u'июня', 7:u'июля', 8:u'августа', 9:u'сентября', 10:u'октября', 11:u'ноября', 12:u'декабря'}
     return u"{0} {1}".format(d.day, months[d.month])
+
+def good_fdate(d):
+    return u"{0} {1} {2}".format(d.day, months[d.month], d.year)
 
 def good_month(m):
     m = int(m)
-    months = {1:u'января', 2:u'февраля', 3:u'марта', 4:u'апреля', 5:u'мая', 6:u'июня', 7:u'июля', 8:u'августа', 9:u'сентября', 10:u'октября', 11:u'ноября', 12:u'декабря'}
     return months[m]
 
+def good_datetime(d):
+    return u"{0} {1}".format(good_fdate(d), d.strftime("%H:%M:%S"))
+
+def good_fdatetime(d):
+    return u"{0} {1}".format(good_fdate(d), d.strftime("%H:%M"))
+
+register.filter('good_datetime', good_datetime)
+register.filter('good_fdatetime', good_fdatetime)
 register.filter('good_date', good_date)
+register.filter('good_fdate', good_fdate)
 register.filter('good_month', good_month)
